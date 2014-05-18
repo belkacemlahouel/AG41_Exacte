6d6548a2a3d0b0af90a7f7a85317898841ecbcdb
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-05-18 19:53:58 +0200

    Ajout des fichiers de la classe Probleme

---
+++ Probleme.cpp
@@ -0,0 +1,5 @@
+#include "Probleme.h"
+
+Probleme::Probleme() {
+	
+}
---
+++ Probleme.h
@@ -0,0 +1,11 @@
+#ifndef __PROBLEME__
+#define __PROBLEME__
+
+#include <iostream>
+
+class Probleme {
+	public:
+		Probleme();
+};
+
+#endif // __PROBLEME__
