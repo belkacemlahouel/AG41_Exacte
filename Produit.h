a423e446a0b8cc0e0f0d753cb35f80f65ffa065e
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-05-18 20:13:31 +0200

    Ajout des fichiers pour la classe Produit

---
+++ Produit.h
@@ -0,0 +1,15 @@
+#ifndef __PRODUIT__
+#define __PRODUIT__
+
+#include <iostream>
+
+using namespace std;
+
+class Produit {
+	private:
+		int i;					// N° du job
+		int di;					// Date due du job i
+		
+};
+
+#endif // __PRODUIT__
