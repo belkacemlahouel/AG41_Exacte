beda78d0f39083adabf70cfbf528f92684989bf5
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-05-18 21:16:22 +0200

    Ajout des fichiers correspondants à la classe Transport

---
+++ Transport.h
@@ -0,0 +1,20 @@
+#ifndef __TRANSPORT__
+#define __TRANSPORT__
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class Transport {
+	private:
+		int c;			// Capacité du transporteur <=> Nombre de produits max
+		int tij[][];	// Temps de transport entre le client i et j, 0..m
+		int eta;		// Coef pour le coût de transport
+
+
+	public:
+		Transport();
+};
+
+#endif // __TRANSPORT__
